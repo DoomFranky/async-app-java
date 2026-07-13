4a8bff1ad87c79f09ddbdcb84f5555f245e2c4b7
Randy <raoulrandy3@gmail.com>
2026-07-13 08:49:42 +0300

    feat: add service image and repository image

---
+++ src/main/java/hei/school/async/repository/ImageRepository.java
@@ -0,0 +1,7 @@
+package hei.school.async.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ImageRepository {
+}
--- src/main/java/hei/school/async/service/ImageService.java
+++ src/main/java/hei/school/async/service/ImageService.java
@@ -1,12 +1,41 @@
 package hei.school.async.service;
 
 import hei.school.async.dto.ImageDTO;
+import hei.school.async.repository.ImageRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-@Service
-public class ImageService {
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
-    public byte[] processAndStoreImage(ImageDTO imageAndEmail) {
-        throw new RuntimeException("Methode not implemented");
+@Service
+@AllArgsConstructor
+public class ImageService {
+    private ImageRepository imageRepository;
+
+    public byte[] processAndStoreImage(ImageDTO imageAndEmail) throws IOException {
+        if (imageAndEmail.getFile().isEmpty()) {
+            throw new IllegalArgumentException("Empty file");
+        }
+
+        BufferedImage originalImage = ImageIO.read(imageAndEmail.getFile().getInputStream());
+
+        BufferedImage bwImage = new BufferedImage(
+                originalImage.getWidth(),
+                originalImage.getHeight(),
+                BufferedImage.TYPE_BYTE_GRAY
+        );
+
+        bwImage.getGraphics().drawImage(originalImage, 0, 0, null);
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(bwImage, "jpg", baos);
+
+        bwImage.flush();
+        originalImage.flush();
+
+        return baos.toByteArray();
     }
 }
