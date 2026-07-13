c1ae879e0e2d41af08495cb47b75367bc9e9c85c
Randy <raoulrandy3@gmail.com>
2026-07-13 08:28:15 +0300

    feat: add image entity

---
+++ src/main/java/hei/school/async/entity/ImageEntity.java
@@ -0,0 +1,25 @@
+package hei.school.async.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class ImageEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String fileName;
+
+    private String email;
+
+    private Instant createdAt;
+}
